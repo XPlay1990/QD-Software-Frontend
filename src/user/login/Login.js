ca3ee18270ce99066889f252c137114ba2df49db
janAdamczyk <j.adamczyk@nicando.de>
2019-07-15 16:12:17 +0200

    login and first table

---
+++ src/user/login/Login.js
@@ -0,0 +1,103 @@
+import React, {Component} from 'react';
+import {login} from '../../util/APIUtils';
+import './Login.css';
+import {Link} from 'react-router-dom';
+import {ACCESS_TOKEN} from '../../constants';
+import loginBackground from '../../resources/login/CHANGE_ME_NOT_FREE.png';
+import abbinoLogo from '../../resources/login/abbino_nicando.png';
+
+
+import {Form, Input, Button, Icon, notification} from 'antd';
+
+const FormItem = Form.Item;
+
+class Login extends Component {
+    render() {
+        const AntWrappedLoginForm = Form.create()(LoginForm)
+        return (
+            <div className={"login-background"}><img src={loginBackground} className="LoginBackground" alt=""/>
+                <div className="login-container">
+                    <h1 className="page-title">Login</h1>
+                    <div className="login-content">
+                        <AntWrappedLoginForm onLogin={this.props.onLogin}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                const loginRequest = Object.assign({}, values);
+                login(loginRequest)
+                    .then(response => {
+
+                        localStorage.setItem(ACCESS_TOKEN, response.accessToken);
+                        this.props.onLogin();
+                    }).catch(error => {
+                    if (error.status === 401) {
+                        notification.error({
+                            message: 'Edi-Portal',
+                            description: 'Your Username or Password is incorrect. Please try again!'
+                        });
+                    } else {
+                        notification.error({
+                            message: 'Edi-Portal',
+                            description: error.message || 'Sorry! Something went wrong. Please try again!'
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit} className="login-form">
+                <FormItem>
+                    {getFieldDecorator('usernameOrEmail', {
+                        rules: [{required: true, message: 'Please input your username or email!'}],
+                    })(
+                        <Input
+                            prefix={<Icon type="user"/>}
+                            size="large"
+                            name="usernameOrEmail"
+                            placeholder="Username or Email"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    {getFieldDecorator('password', {
+                        rules: [{required: true, message: 'Please input your Password!'}],
+                    })(
+                        <Input
+                            prefix={<Icon type="lock"/>}
+                            size="large"
+                            name="password"
+                            type="password"
+                            placeholder="Password"/>
+                    )}
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" htmlType="submit" size="large" className="login-form-button">Login</Button>
+                    Or <Link to="/signup">register now!</Link>
+                </FormItem>
+                <div className={"LoginFooter"}>
+                    <img src={abbinoLogo} className="LoginBackground" alt=""/>
+                </div>
+            </Form>
+        );
+    }
+}
+
+
+export default Login;
