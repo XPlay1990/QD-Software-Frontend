cbce57efff290c9dbfc12ac8646a84a129a3bcbd
Jan Adamczyk <j_adamczyk@hotmail.com>
2019-11-22 16:47:02 +0100

    attachment icons

--- src/edi/display/detail/attachments/Attachment.js
+++ src/edi/display/detail/attachments/Attachment.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import "./Attachment.css"
-import {ReactComponent as PdfIcon} from "../../../../resources/fileIcons/pdf.svg"
-import {ReactComponent as TxtIcon} from "../../../../resources/fileIcons/txt.svg"
-import {ReactComponent as PngIcon} from "../../../../resources/fileIcons/png.svg"
-import {ReactComponent as JpgIcon} from "../../../../resources/fileIcons/jpg.svg"
-import {ReactComponent as DocxIcon} from "../../../../resources/fileIcons/docx.svg"
-import {ReactComponent as DefaultIcon} from "../../../../resources/fileIcons/clip.svg"
+// import {ReactComponent as PdfIcon} from "../../../../resources/fileIcons/pdf.svg"
+// import {ReactComponent as TxtIcon} from "../../../../resources/fileIcons/txt.svg"
+// import {ReactComponent as PngIcon} from "../../../../resources/fileIcons/png.svg"
+// import {ReactComponent as JpgIcon} from "../../../../resources/fileIcons/jpg.svg"
+// import {ReactComponent as DocxIcon} from "../../../../resources/fileIcons/docx.svg"
+// import {ReactComponent as DefaultIcon} from "../../../../resources/fileIcons/clip.svg"
+import {ReactComponent as DownloadIcon} from "../../../../resources/fileIcons/md-download.svg"
 import {ACCESS_TOKEN} from "../../../../config/constants";
+import Button from "@material-ui/core/Button";
 
 // import Divider from '@material-ui/core/Divider';
 
@@ -46,52 +48,57 @@ class Attachment extends Component {
     }
 
     render() {
-        let fileSymbol;
-        switch (this.state.fileType) {
-            //TODO: LICENSE FLATICONS
-            case "application/pdf":
-                fileSymbol =
-                    <PdfIcon
-                        className="EdiFileIcon"
-                        onClick={this.downloadFile}
-                    />;
-                break;
-            case "text/plain":
-                fileSymbol =
-                    <TxtIcon
-                        className="EdiFileIcon"
-                        onClick={this.downloadFile}
-                    />;
-                break;
-            case "image/png":
-                fileSymbol =
-                    <PngIcon
-                        className="EdiFileIcon"
-                        onClick={this.downloadFile}
-                    />;
-                break;
-            case "image/jpeg":
-                fileSymbol =
-                    <JpgIcon
-                        className="EdiFileIcon"
-                        onClick={this.downloadFile}
-                    />;
-                break;
-            case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-                fileSymbol =
-                    <DocxIcon
-                        className="EdiFileIcon"
-                        onClick={this.downloadFile}
-                    />;
-                break;
-            default:
-                fileSymbol =
-                    <DefaultIcon
-                        className="EdiFileIcon"
-                        onClick={this.downloadFile}
-                    />;
-                break;
-        }
+        let fileSymbol =
+            <Button onClick={this.downloadFile} className="EdiFileIcon">
+                <DownloadIcon/>
+            </Button>;
+
+
+        // switch (this.state.fileType) {
+        //     //TODO: LICENSE FLATICONS
+        //     case "application/pdf":
+        //         fileSymbol =
+        //             <PdfIcon
+        //                 className="EdiFileIcon"
+        //                 onClick={this.downloadFile}
+        //             />;
+        //         break;
+        //     case "text/plain":
+        //         fileSymbol =
+        //             <TxtIcon
+        //                 className="EdiFileIcon"
+        //                 onClick={this.downloadFile}
+        //             />;
+        //         break;
+        //     case "image/png":
+        //         fileSymbol =
+        //             <PngIcon
+        //                 className="EdiFileIcon"
+        //                 onClick={this.downloadFile}
+        //             />;
+        //         break;
+        //     case "image/jpeg":
+        //         fileSymbol =
+        //             <JpgIcon
+        //                 className="EdiFileIcon"
+        //                 onClick={this.downloadFile}
+        //             />;
+        //         break;
+        //     case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        //         fileSymbol =
+        //             <DocxIcon
+        //                 className="EdiFileIcon"
+        //                 onClick={this.downloadFile}
+        //             />;
+        //         break;
+        //     default:
+        //         fileSymbol =
+        //             <DefaultIcon
+        //                 className="EdiFileIcon"
+        //                 onClick={this.downloadFile}
+        //             />;
+        //         break;
+        // }
         return (
             <div className="Attachment">
                 {fileSymbol}
