a00a79ef3da0dfd3f1816be81520014029156d9b
Jan Adamczyk <j_adamczyk@hotmail.com>
2019-11-28 00:03:41 +0100

    ediconnection readby first steps

--- src/edi/display/ColumnConfig.js
+++ src/edi/display/ColumnConfig.js
@@ -7,16 +7,22 @@ export const columnConfig = [
         accessor: 'customer.name',
         headerStyle: {whiteSpace: 'unset'},
         style: {whiteSpace: 'unset'},
+        // classname: ${e.original.id} === 1? 'test':null,
         // Cell: e => <Link state={this.state} to={`/edi_connection/${e.original.ediConnectionId}`}> {e.value} </Link>
         // TODO: pass state, so that link can be protectedroute @ APP.js
-        Cell: e => <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+        // Cell: e => <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+        Cell: e => e.original.read ?
+            <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+            : <Link to={`/edi_connection/${e.original.id}`}> <b>{e.value}</b> </Link>
     },
     {
         Header: 'Supplier',
         accessor: 'supplier.name',
         headerStyle: {whiteSpace: 'unset'},
         style: {whiteSpace: 'unset'},
-        Cell: e => <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+        Cell: e => e.original.read ?
+            <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+            : <Link to={`/edi_connection/${e.original.id}`}> <b>{e.value}</b> </Link>
     },
     {
         Header: 'State',
@@ -24,13 +30,17 @@ export const columnConfig = [
         headerStyle: {whiteSpace: 'unset'},
         style: {whiteSpace: 'unset'},
         maxWidth: 150,
-        Cell: e => <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+        Cell: e => e.original.read ?
+            <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+            : <Link to={`/edi_connection/${e.original.id}`}> <b>{e.value}</b> </Link>
     },
     {
         Header: 'Last Modified',
         accessor: 'updateTime',
         headerStyle: {whiteSpace: 'unset'},
         style: {whiteSpace: 'unset'},
-        Cell: e => <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+        Cell: e => e.original.read ?
+            <Link to={`/edi_connection/${e.original.id}`}> {e.value} </Link>
+            : <Link to={`/edi_connection/${e.original.id}`}> <b>{e.value}</b> </Link>
     },
 ];
